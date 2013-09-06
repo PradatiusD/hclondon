9bcb7f4c948ecef34caf6010fe1df07da2c6a0ba
PradatiusD <danielprada2012@gmail.com>
2013-09-06 15:47:48 -0400

    added original footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Theme footer.
+ */
+?>
+
+	<div class="clearfix"></div>
+
+	<!-- Prompt IE 6 users to install Chrome Frame -->
+	<!--[if lt IE 7 ]>
+		<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+		<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+	<![endif]-->
+
+	<?php wp_footer(); ?>
+	
+</body>
+</html>
